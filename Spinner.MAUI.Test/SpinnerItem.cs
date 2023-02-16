e990dfdfb7fbdbc06ac406f1c9bacbf16e50aa29
Hector Alonso <hjam27@outlook.es>
2023-02-16 13:37:05 +0100

    SpinnerItem public

--- Spinner.MAUI.Test/SpinnerItem.cs
+++ Spinner.MAUI.Test/SpinnerItem.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Spinner.MAUI.Test;
 
-namespace Spinner.MAUI.Test
+public class SpinnerItem : ISpinnerItem
 {
-    internal class SpinnerItem : ISpinnerItem
-    {
-        public ImageSource ImageSource { get; set; }
-        public string Text { get; set; } = string.Empty;
-        public object Item { get; set; }
-    }
+    public ImageSource ImageSource { get; set; }
+    public string Text { get; set; } = string.Empty;
+    public object Item { get; set; }
 }
